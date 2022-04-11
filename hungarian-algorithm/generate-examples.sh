04a86b1ec57405e1c919eb94ce8a5c07bd2e3437
Jakub Michalak <michalakjakub1999@gmail.com>
2022-04-11 16:55:31 +0200

    Add some usefule make targets

--- hungarian-algorithm/generate-examples.sh
+++ hungarian-algorithm/generate-examples.sh
@@ -4,5 +4,5 @@ set -euxo pipefail
 
 for i in 10 20 50 100 200 500 1000 2000 5000
 do
-   dotnet run --project example-generator -- $i -p 1 examples/$i.txt 
+   ./publish/example-generator -- $i -p 1 examples/$i.txt
 done
