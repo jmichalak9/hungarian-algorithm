db182138bfb9956ba4edd80c8a3c9c02d0b76bfc
Jakub Michalak <michalakjakub1999@gmail.com>
2022-04-11 14:53:41 +0200

    Add example generator with examples

---
+++ hungarian-algorithm/example-generator/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using CommandLine;
+
+namespace ExampleGenerator
+{
+    class Program
+    {
+        public class Options
+        {
+            [Value(0)]
+            public int N { get; set; }
+            [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
+            public bool Verbose { get; set; }
+            [Option("rand-min", Required = false, Default = 0, HelpText = "Minimal random weight.")]
+            public int RandMin { get; set; }
+            [Option("rand-max", Required = false, Default = 10, HelpText = "Maximal random weight.")]
+            public int RandMax { get; set; }
+            [Option('p', "edges-prob", Required = false, Default = 0.8f, HelpText = "Probability of existing edges.")]
+            public float EdgesProb { get; set; }
+            [Value(1)]
+            public string Filename { get; set; }
+
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                var rand = new Random();
+                int randMin = 0, randMax = 0;
+                float edgesProb = 0;
+                var n = 0;
+                string filename = "";
+                Parser.Default.ParseArguments<Options>(args)
+                    .WithParsed<Options>(o =>
+                    {
+                        randMin = o.RandMin;
+                        randMax = o.RandMax;
+                        n = o.N;
+                        edgesProb = o.EdgesProb;
+                        filename = o.Filename;
+                    });
+                var edges = new int[n, n];
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (rand.NextDouble() > edgesProb)
+                            edges[i, j] = -1;
+                        else
+                            edges[i, j] = rand.Next(randMin, randMax);
+                    }
+                }
+                using (StreamWriter writer = new StreamWriter(filename))
+                {
+                    writer.WriteLine($"{n}");
+                    for (int i = 0; i < n; i++)
+                    {
+                        for (int j = 0; j < n; j++)
+                        {
+                            writer.Write($"{edges[i,j]} ");
+                        }
+
+                        writer.WriteLine();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"error: {e}");
+            }
+        }
+    }
+}
---
+++ hungarian-algorithm/generate-examples.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+for i in 10 20 50 100 200 500 1000 2000 5000
+do
+   dotnet run --project example-generator -- $i -p 1 examples/$i.txt 
+done
